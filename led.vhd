42ea549da278d3ae5115cd3603305e551e77fb90
Istanolion <akuma_diego@hotmail.com>
2018-11-13 14:00:52 -0600

    p1

--- led.vhd
+++ led.vhd
@@ -3,11 +3,23 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity led is
     Port ( entrada : in  STD_LOGIC;
-           salida : out  STD_LOGIC);
+           salida : out  Integer);
 end led;
 
 architecture Behavioral of led is
-
+constant max_count: INTEGER := 16;
+signal count: INTEGER range 0 to max_count;
 begin
-	salida<=entrada;
+	process (entrada, count)
+	begin
+	if entrada'event and entrada='1' then
+			if count < max_count then 
+				count <= count+1;
+			else
+				count <= 0;
+			end if;
+		end if;
+	salida<=count;
+	end process;
+
 end Behavioral;
---
+++ led.vhd.bak
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity led is
+    Port ( entrada : in  STD_LOGIC;
+           salida : out  STD_LOGIC);
+end led;
+
+architecture Behavioral of led is
+
+begin
+	salida<=entrada;
+end Behavioral;
